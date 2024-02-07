a3de2b65a294934378938defb9a78ed3b46bfe3b
Vanya Robertson <vanya.robertson@icloud.com>
2024-02-07 11:20:50 +0000

    added install script

---
+++ verkman-install-cli.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# changes keymap on installer to verkman
+
+# select keymap for installation
+[[ -e /usr/share/kbd/keymaps/i386/workman ]] || mkdir /usr/share/kbd/keymaps/i386/workman
+[[ -f $(pwd)-verkman.map ]] && cp $(pwd)/verkman.map /usr/share/kbd/keymaps/i386/workman/
+loadkeys verkman
