1d325e6c8864825e6c3d5afbcd9b6db6ec27b46d
Vanya Robertson <vanya.robertson@icloud.com>
2024-02-10 11:38:57 +0000

    corrected typo

--- verkman-install-cli.sh
+++ verkman-install-cli.sh
@@ -4,5 +4,5 @@
 
 # select keymap for installation
 [[ -e /usr/share/kbd/keymaps/i386/workman ]] || mkdir /usr/share/kbd/keymaps/i386/workman
-[[ -f $(pwd)-verkman.map ]] && cp $(pwd)/verkman.map /usr/share/kbd/keymaps/i386/workman/
+[[ -f $(pwd)/verkman.map ]] && cp $(pwd)/verkman.map /usr/share/kbd/keymaps/i386/workman/
 loadkeys verkman
