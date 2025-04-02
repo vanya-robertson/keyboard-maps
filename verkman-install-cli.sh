b69d53b4696c6708e931e3de34a4523ed21e9a51
Vanya Robertson <vanya.robertson@icloud.com>
2025-04-02 15:02:25 +0100

    added comment: only works on arch-based distros

--- verkman-install-cli.sh
+++ verkman-install-cli.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# changes keymap on installer to verkman
+# changes keymap on installer to verkman on arch-based systems
 
 # select keymap for installation
 [[ -e /usr/share/kbd/keymaps/i386/workman ]] || mkdir /usr/share/kbd/keymaps/i386/workman
-[[ -f $(pwd)/verkman.map ]] && cp $(pwd)/verkman.map /usr/share/kbd/keymaps/i386/workman/
+[[ -f $(pwd)/verkman.map ]] && cp "$(pwd)"/verkman.map /usr/share/kbd/keymaps/i386/workman/
 loadkeys verkman
