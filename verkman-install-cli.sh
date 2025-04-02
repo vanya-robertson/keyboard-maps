68e04fda9a2075f9255876c19b5b87ad965e389a
Vanya Robertson <vanya.robertson@icloud.com>
2025-04-02 15:03:53 +0100

    added comfortable keyboard delay and rate

--- verkman-install-cli.sh
+++ verkman-install-cli.sh
@@ -6,3 +6,4 @@
 [[ -e /usr/share/kbd/keymaps/i386/workman ]] || mkdir /usr/share/kbd/keymaps/i386/workman
 [[ -f $(pwd)/verkman.map ]] && cp "$(pwd)"/verkman.map /usr/share/kbd/keymaps/i386/workman/
 loadkeys verkman
+kbdrate -d 300 -r 65
